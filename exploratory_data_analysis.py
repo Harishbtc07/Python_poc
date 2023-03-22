887c3f3bf03cde53a4a2c2bc3cdea2be0059139d
Harishbtc07 <harishr@boston-technology.com>
2023-03-22 18:54:53 +0530

    python_poc

--- exploratory_data_analysis.py
+++ exploratory_data_analysis.py
@@ -107,11 +107,15 @@ from sklearn.model_selection import train_test_split
 x_train,x_test,y_train,y_test=train_test_split(x,y,test_size=0.2,random_state=2)
 
 #Training and predicting
-from sklern.linear_model import LogisticRegression
+from sklearn.linear_model import LogisticRegression
 
 logmodel=LogisticRegression()
 logmodel.fit(x_train,y_train)
 
 predictions=logmodel.predict(x_test)
+print(predictions)
 
+from sklearn.metrics import accuracy_score
+accuracy=accuracy_score(y_test,predictions)
+print(accuracy)
 
